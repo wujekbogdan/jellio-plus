9afcfdb4dfc65e9100ba60a88a618680986e2722
wujekbogdan <533954+wujekbogdan@users.noreply.github.com>
2026-05-22 10:28:37 +0200

    chore: add vitest with happy-dom

---
+++ jellio-web/vitest.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, './src'),
+    },
+  },
+  test: {
+    environment: 'happy-dom',
+  },
+});
